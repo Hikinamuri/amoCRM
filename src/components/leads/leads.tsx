b79dcd7df36738c72c8c09162aebba0ff7aaf6b8
Hikinamuri <Hikinamuri@mail.ru>
2024-09-14 04:05:10 +0300

    fix: .env

--- src/components/leads/leads.tsx
+++ src/components/leads/leads.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useEffect, useMemo, useState } from "react";
 import { Lead, Task } from "../..//types/types";
 import LeadsAPI from "../../api/LeadsAPI";
--- src/pages/GeneralPage/GeneralPage.tsx
+++ src/pages/GeneralPage/GeneralPage.tsx
@@ -5,7 +5,7 @@ const GeneralPage = () => {
     const authUser = localStorage.getItem("accessToken")
 
     const getAuthorizationToken = () => {
-        window.location.href = 'https://www.amocrm.ru/oauth?client_id=38d7c8d2-61f8-4f34-a27f-2bfb59f749c3&state={state}&mode=popup';
+        window.location.href = `https://www.amocrm.ru/oauth?client_id=${import.meta.env.VITE_CLIENT_ID}&state={state}&mode=popup`;
     }
 
     useEffect(() => {
